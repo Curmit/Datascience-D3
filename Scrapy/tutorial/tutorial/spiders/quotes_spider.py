12baf9df301776597ecfe067e72e7c006367e09a
ricklen <ricklenderink93@gmail.com>
2018-02-27 14:29:36 +0100

    Scrapy Spider
    execution: scrapy crawl quotes -o vacatures.json -a tag="vacaturenaam"

---
+++ Scrapy/tutorial/tutorial/spiders/quotes_spider.py
@@ -0,0 +1,35 @@
+import scrapy
+
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
+
+    def start_requests(self):
+        url = 'http://www.indeed.nl/'
+        tag = getattr(self,'tag',None)
+        tags = tag.split(" ")
+        for i in range(0,len(tags)):
+            tag = tags[i] + "-"
+            tags[i] = tag
+        tags.append("vacatures")
+        for i in tags:
+            url += str(i)
+        yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        print(str(self))
+        vacature = response.css('.result')[0]
+        titles = vacature.xpath('//h2[contains(@class, "jobtitle")]/a/@title').extract()
+        locations = vacature.xpath('//div[contains(@data-tn-component, "organicJob")]/span[contains(@class, "location")]/text()').extract()
+        #companies = vacature.xpath('//div[contains(@data-tn-component, "organicJob")]/span[contains(@class, "company")]/text()').extract()
+        for i in range(0,len(titles)):
+            #print("list size: " + str(len(titles)) + "locations size: " + str(len(locations)) + "companies size: " + str(len(companies)))
+            yield {
+                'jobTitle': titles[i],
+                'location': locations[i],
+        #        'company': companies[i],
+            }
+
+        next_page = response.xpath('//span[@class="np" and contains(.,"Volgende")]/../../@href').extract_first()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
---
+++ Scrapy/tutorial/urlbuilder.py
@@ -0,0 +1,4 @@
+import scrapy
+
+scrapy crawl quotes -o vacatures-data-scientist.json -a tag="data scientist"
+scrapy crawl quotes -o vacatures-business-intelligence.json -a tag="business business intelligence"
